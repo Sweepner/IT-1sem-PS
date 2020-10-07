b6e23bff3fed09359683e67a13b53427c4cc1d77
Sweepner <rodzonraf@gmail.com>
2020-10-07 15:25:02 +0200

    do while

---
+++ what is your name/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int a;
+    cout << "How old are you?" << endl;
+    cin>>a;
+    if(a>=18)
+    {
+        cout<<"You are adult"<<endl;
+
+    }
+    else
+    {
+        cout<<"You are not adult"<<endl;
+    }
+
+
+    return 0;
+}
