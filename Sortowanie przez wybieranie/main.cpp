192a47d1256834eb4a8e3cd59cf32cda534db748
Sweepner <rodzonraf@gmail.com>
2020-10-11 00:56:05 +0200

    sortowanie przez wybieranie - gotowe

--- Sortowanie przez wybieranie/main.cpp
+++ Sortowanie przez wybieranie/main.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-int find_min(int tab[], int n){
-    int j = 0;
-    for(int i = 0; i < n; i++){
+int find_min(int tab[], int n, int start){
+    int j = start;
+    for(int i = start; i < n; i++){
         if(tab[j]>tab[i]){
         j=i;
         }
@@ -24,22 +24,36 @@ int main()
 {   const int N=10;
     int i, tab[N];
     srand(time(0));
-    for(i=0; i < 10 ; i++){
+    for(i=0; i < N ; i++){
         tab[i]=( rand() % 10 );
     //  cout << tab[i] << endl;
 
     }
 
-    int my_min=find_min(tab, N);
+
    // cout << my_min << endl;
-    for(i=0; i < 10; i++){
+    for(i=0; i < N; i++){
+        int my_min=find_min(tab, N, i);
         my_swap(tab, i, my_min);
-        my_min=find_min(tab, N);
-
-
-
     }
+    cout << "[";
+    for(i=0 ; i < N ; i++){
+        cout << tab[i] << ",";
+    }
+    cout << "]" << endl;
 
-
+ /*   for(int i = 0; i < N; i++) {
+        int min_index = i;
+        int min_value = tab[i];
+        for(int j = i; j < N; j++) {
+            if(tab[j] < min_value){
+                min_value = tab[j];
+                min_index = j;
+            }
+        }
+        int tmp = tab[i];
+        tab[i] = tab[min_index];
+        tab[min_index] = tmp;
+    } */
     return 0;
 }
